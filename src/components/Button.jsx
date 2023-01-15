f725278650852cdee06cc18a7785bd95b77454c5
Arpit Verma <arpitv970@gmail.com>
2023-01-15 22:10:54 +0530

    Created business section of the React App

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
 
-const Button = () => {
-  return (
-    <div>Button</div>
-  )
-}
+const Button = ({ styles }) => {
+    return (
+        <button
+            type='button'
+            className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none rounded-xl hover:rounded-[2rem] transition-all duration-300 ease-in-out ${styles}`}
+        >
+            Get Started
+        </button>
+    );
+};
 
-export default Button
+export default Button;
