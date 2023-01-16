68135cdec3b98ef61bd8d360e5e4804ed3833601
Arpit Verma <arpitv970@gmail.com>
2023-01-16 10:40:38 +0530

    Created billing section of the React App

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import { apple, bill, google } from '../assets';
+import styles, { layout } from '../styles';
 
 const Billing = () => {
-  return (
-    <div>Billing</div>
-  )
-}
+    return (
+        <section id='product' className={layout.sectionReverse}>
+            <div className={layout.sectionImgReverse}>
+                <img
+                    src={bill}
+                    alt='billing'
+                    className='w-[100%] h-[100%] relative z-[5]'
+                />
+                <div className='absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient' />
+                <div className='absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient' />
+            </div>
 
-export default Billing
+            <div className={layout.sectionInfo}>
+                <h2 className={styles.heading2}>
+                    Easily control your <br className='sm:block hidden' />{' '}
+                    billing & invoicing.
+                </h2>
+                <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+                    Arcu tortor, purus in mattis at sed integer faucibus.
+                    Aliquet quis aliquet eget mauris tortor.ç Aliquet ultrices
+                    ac, ametau.
+                </p>
+
+                <div className='flex flex-row flex-wrap sm:mt-10 mt-6'>
+                    <img
+                        src={apple}
+                        alt='apple_store'
+                        className='w-[128px] h-[42px] object-contain mr-5 cursor-pointer'
+                    />
+                    <img
+                        src={google}
+                        alt='google_play'
+                        className='w-[128px] h-[42px] object-contain cursor-pointer'
+                    />
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Billing;
